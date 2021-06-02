04db946c9e7fdd88d19d3774c2cdd03909bbb97a
Ronaldo Michuy Valera <66487844+Website14-rnmv@users.noreply.github.com>
2021-06-02 18:21:35 -0500

    Add files via upload

---
+++ animations/animateScroll.js
@@ -0,0 +1,34 @@
+window.sr = ScrollReveal()
+
+/*---------------------------INICIO DE CABECERA Y NAV--------------------------*/
+sr.reveal('.animacionBottom',{
+    duration : 2000,
+    origin : 'bottom',
+    distance : '400px',
+    
+
+});  
+sr.reveal('.animacionTop',{
+    duration : 1500,
+    origin : 'top',
+    distance : '400px',
+    
+  
+
+});     
+sr.reveal('.foo',{
+    duration: 2000,
+}
+);
+sr.reveal('.foo2',{
+    duration: 3000,
+   
+}
+);
+
+sr.reveal('.cuadros-animate',{
+    duration: 2000,
+    
+}
+);
+
